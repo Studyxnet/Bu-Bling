af9d33f3bb0f0cd3df5374d466f6e3482190e2da
Rodrigo Amaro <jbravo.br@gmail.com>
2015-11-21 17:05:31 -0200

    Primeiro Commit

---
+++ Droid/Services/PromoServices.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Android.Widget;
+using Android.App;
+using Android.Util;
+using Xamarin.Forms;
+using TaskStackBuilder = Android.Support.V4.App.TaskStackBuilder;
+using Android.Support.V4.App;
+
+namespace bubling.Droid
+{
+    [Service]
+    public class PromoServices : Service
+    {
+        System.Threading.Timer _timer;
+
+        public override StartCommandResult OnStartCommand(Android.Content.Intent intent, StartCommandFlags flags, int startId)
+        {
+            Log.Debug("PromoService", "PromoService iniciado");
+            CallWebService();
+
+            return base.OnStartCommand(intent, flags, startId);
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            _timer.Dispose();
+            Log.Debug("PromoService", "PromoService parado");       
+        }
+
+        public void CallWebService()
+        {
+            _timer = new System.Threading.Timer((o) =>
+                {
+                    // Construindo a notificação
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                        .SetAutoCancel(true)
+                        .SetContentTitle("Aviso")
+                        .SetSmallIcon(Resource.Drawable.notification)
+                        .SetContentText("Mensagem por Notificação");
+
+                    // Publicando a notificação
+                    NotificationManager notificationManager = 
+                        (NotificationManager)GetSystemService("notification");
+                    notificationManager.Notify(10, builder.Build());
+
+                }, null, 0, 4000);
+        }
+
+        public override Android.OS.IBinder OnBind(Android.Content.Intent intent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ bubling/Pages/Home.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace bubling
+{
+    public class Home : ContentPage
+    {
+        private WebView _webView;
+        private StackLayout _mainLayout;
+
+        public Home(string url)
+        {
+            this.Title = "Home";
+
+            if (!String.IsNullOrEmpty(url))
+            {
+                this._webView = new WebView
+                { 
+                    Source = url, 
+                    HeightRequest = 1000,
+                    WidthRequest = 1000
+                };
+
+                this._mainLayout = new StackLayout
+                {
+                    Children = { _webView }
+                };
+
+                this.Content = _mainLayout;
+            }
+            else
+                DisplayAlert("Aviso", "Erro ao carregar conteúdo", "OK");
+        }
+    }
+}
+
+
